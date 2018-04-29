559d1f710f217fd46adbdd8cb4b1fa194cfb1a20
csamuelsson <c.samuelsson94@gmail.com>
2018-04-29 17:35:53 +0200

    Changed main function and data pipeline to work with drug data instead of MNIST

--- bayesianNN.py
+++ bayesianNN.py
@@ -2,16 +2,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-
 # only for debugging purposes
 import sys
 import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning) # doesn't work?
+warnings.filterwarnings("ignore") # doesn't work?
 
 # Dependencies
-from absl import flags
-# Local errors with matplotlib, comment out when debugged
 import matplotlib
 matplotlib.use("Agg")
 from matplotlib import figure  # pylint: disable=g-import-not-at-top
@@ -19,9 +15,22 @@ from matplotlib.backends import backend_agg
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
+import pandas as pd
+from flags import *
+from utils import *
 
 # importing mnist dataset for demo purpose
 from tensorflow.contrib.learn.python.learn.datasets import mnist # remove
+
+data = pd.read_csv("variables.csv.txt", sep='\t')
+
+# last column is the response variable
+y = np.array(data[data.columns[-1]])
+# rest of the variables (except the first) are the features
+x = np.array(data[data.columns[1:-1]])
+
+# since x_6850 -> x_6908 has variance 0 they don't contribute to our mapping, therefore we remove them here
+x = x[0:len(x), 0:6849]
 
 # What's happening here? Do we need to care?
 # TODO(b/78137893): Integration tests currently fail with seaborn imports.
@@ -34,53 +43,16 @@ except ImportError:
 tfd = tf.contrib.distributions
 IMAGE_SHAPE = [28, 28]
 
-flags.DEFINE_float("learning_rate",
-                   default=0.1,
-                   help="Initial learning rate.")
-flags.DEFINE_integer("max_epochs",
-                     default=6000,
-                     help="Number of training epochs to run.")
-flags.DEFINE_list("layer_sizes",
-                  default=["128", "128", "128"],
-                  help="Comma-separated list denoting hidden units per layer.")
-flags.DEFINE_string("activation",
-                    default="relu",
-                    help="Activation function for all hidden layers.")
-flags.DEFINE_integer("batch_size",
-                     default=128,
-                     help="Batch size. Must divide evenly into dataset sizes.")
-flags.DEFINE_string("data_dir",
-                    default=os.path.join(os.getenv("TEST_TMPDIR", "/tmp"),
-                                         "bayesian_neural_network/data"),
-                    help="Directory where data is stored (if using real data).") # ?
-flags.DEFINE_string(
-    "model_dir",
-    default=os.path.join(os.getenv("TEST_TMPDIR", "/tmp"),
-                         "bayesian_neural_network/"),
-    help="Directory to put the model's fit.") # ?
-flags.DEFINE_integer("viz_epochs",
-                     default=400,
-                     help="Frequency at which save visualizations.")
-flags.DEFINE_boolean("viz_enabled",
-                      default=True,
-                      help="Whether vizualisations should be generated or not")
-
-flags.DEFINE_integer("num_monte_carlo",
-                     default=100,
-                     help="Network draws to compute predictive probabilities.")
-
-flags.DEFINE_float("prior_std",
-                   default=1.0,
-                   help="Standard deviation for the prior distribution over the weights")
-
+# Tuning some of the hyperparameters manually:
 FLAGS = flags.FLAGS
 FLAGS.learning_rate = 0.5
 FLAGS.viz_epochs = 1000
 FLAGS.viz_enabled = False # set to false if we want to train the model faster
 FLAGS.max_epochs = 10000
 
-# ?
-def plot_weight_posteriors(names, qm_vals, qs_vals, fname):
+train_percentage = 0.8
+
+'''def plot_weight_posteriors(names, qm_vals, qs_vals, fname):
   """Save a PNG plot with histograms of weight means and stddevs.
   Args:
     names: A Python `iterable` of `str` variable names.
@@ -112,10 +84,9 @@ def plot_weight_posteriors(names, qm_vals, qs_vals, fname):
 
   fig.tight_layout()
   canvas.print_figure(fname, format="png")
-  print("saved {}".format(fname))
+  print("saved {}".format(fname))'''
 
-# ?
-def plot_heldout_prediction(input_vals, probs,
+'''def plot_heldout_prediction(input_vals, probs,
                             fname, n=10, title=""):
   """Save a PNG plot visualizing posterior uncertainty on heldout data.
   Args:
@@ -148,25 +119,47 @@ def plot_heldout_prediction(input_vals, probs,
   fig.tight_layout()
 
   canvas.print_figure(fname, format="png")
-  print("saved {}".format(fname))
+  print("saved {}".format(fname))'''
 
 
-# Data pipeline - what's happening here?
-def build_input_pipeline(mnist_data, batch_size, heldout_size):
-  """Build an Iterator switching between train and heldout data."""
+def build_input_pipeline(drug_data_path, batch_size, heldout_size,
+                          number_of_principal_components):
+  """Build an Iterator switching between train and heldout data.
+  Args:
+    `drug_data`: string representing the path to the .npy dataset.
+    `batch_size`: integer specifying the batch_size for the dataset.
+    `heldout_size`: ?
+    `number_of_principal_components`: integer specifying how many principal components
+    to reduce the dataset into.
+  """
   # Build an iterator over training batches.
-  training_dataset = tf.data.Dataset.from_tensor_slices(
-      (mnist_data.train.images, np.int32(mnist_data.train.labels)))
-    # Shuffle the dataset (note shuffle argument larger than training size)
-    # and form batches of size `batchsize`
+  # Load the training data into two NumPy arrays, for example using `np.load()`.
+  with np.load("/data/drug_data.npz") as data:
+    features = data["features"]
+    labels = data["labels"]
+
+    # PCA (sklearn) and normalising
+
+    # Splitting into training and validation sets
+    train_range = int(train_percentage * len(x))
+
+    training_features = features[:train_range]
+    training_labels = labels[:train_range]
+    validation_features = features[train_range:]
+    validation_labels = validation[train_range:]
+
+  # Create the tf.Dataset object
+  training_dataset = tf.data.Dataset.from_tensor_slices((training_features, training_labels))
+
+  # Shuffle the dataset (note shuffle argument larger than training size)
+  # and form batches of size `batch_size`
   training_batches = training_dataset.shuffle(2000).repeat().batch(batch_size)
   training_iterator = training_batches.make_one_shot_iterator()
 
   # Build a iterator over the heldout set with batch_size=heldout_size,
   # i.e., return the entire heldout set as a constant.
   heldout_dataset = tf.data.Dataset.from_tensor_slices(
-      (mnist_data.validation.images,
-       np.int32(mnist_data.validation.labels)))
+      (validation_features, validation_labels))
   heldout_frozen = (heldout_dataset.take(heldout_size).
                     repeat().batch(heldout_size))
   heldout_iterator = heldout_frozen.make_one_shot_iterator()
@@ -176,9 +169,9 @@ def build_input_pipeline(mnist_data, batch_size, heldout_size):
   handle = tf.placeholder(tf.string, shape=[])
   feedable_iterator = tf.data.Iterator.from_string_handle(
       handle, training_batches.output_types, training_batches.output_shapes)
-  images, labels = feedable_iterator.get_next()
+  features_final, labels_final = feedable_iterator.get_next()
 
-  return images, labels, handle, training_iterator, heldout_iterator
+  return features_final, labels_final, handle, training_iterator, heldout_iterator
 
 
 def main(argv):
@@ -188,7 +181,7 @@ def main(argv):
   # extract the activation function from the hyperopt spec as an attribute from the tf.nn module
   FLAGS.activation = getattr(tf.nn, FLAGS.activation)
 
-  # Not sure what's happening here? but probably nothing too important
+  # Tracking whether we are overwriting an old log directory or not
   if tf.gfile.Exists(FLAGS.model_dir):
     tf.logging.warning(
         "Warning: deleting old log directory at {}".format(FLAGS.model_dir))
@@ -196,7 +189,7 @@ def main(argv):
   tf.gfile.MakeDirs(FLAGS.model_dir)
 
   # Load MNIST data
-  mnist_data = mnist.read_data_sets(FLAGS.data_dir)
+  # mnist_data = mnist.read_data_sets(FLAGS.data_dir)
 
   # define the graph
   with tf.Graph().as_default():
@@ -215,28 +208,29 @@ def main(argv):
             units=units,
             activation=FLAGS.activation,
             trainable=True,
-            kernel_prior_fn=tfp.layers.default_multivariate_normal_fn, # NormalDiag with Xavier
+            kernel_prior_fn=default_multivariate_normal_fn, # NormalDiag with hyperopt sigma
+            # kernel_prior_fn=make_scale_mixture_prior_fn, # pls work
             kernel_posterior_fn=tfp.layers.default_mean_field_normal_fn(), # softplus(sigma)
             kernel_posterior_tensor_fn=lambda x: x.sample(),
-            bias_prior_fn=tfp.layers.default_multivariate_normal_fn, # NormalDiag with Xavier
+            bias_prior_fn=tfp.layers.default_multivariate_normal_fn, # NormalDiag with hyperopt sigma
             bias_posterior_fn=tfp.layers.default_mean_field_normal_fn(), # softplus(sigma)
             bias_posterior_tensor_fn=lambda x: x.sample()
             )
         neural_net.add(layer)
       neural_net.add(tfp.layers.DenseReparameterization(
-        units=10, # change to 1
+        units=1, # change to 1
         activation=None,
         trainable=True,
-        kernel_prior_fn=tfp.layers.default_multivariate_normal_fn, # NormalDiag with Xavier?
+        kernel_prior_fn=default_multivariate_normal_fn, # NormalDiag with hyperopt sigma
         kernel_posterior_fn=tfp.layers.default_mean_field_normal_fn(), # softplus(sigma)
         kernel_posterior_tensor_fn=lambda x: x.sample(),
-        bias_prior_fn=tfp.layers.default_multivariate_normal_fn, # NormalDiag with Xavier?
+        bias_prior_fn=default_multivariate_normal_fn, # NormalDiag with hyperopt sigma
         bias_posterior_fn=tfp.layers.default_mean_field_normal_fn(), # softplus(sigma)
         bias_posterior_tensor_fn=lambda x: x.sample()
         ))
-      logits = neural_net(images) # remove
-      # predictions = neural_net(images)
-      labels_distribution = tfd.Categorical(logits=logits) # remove
+      # logits = neural_net(images) # remove
+      predictions = neural_net(images)
+      # labels_distribution = tfd.Categorical(logits=logits) # remove
 
       # Extract weight posterior statistics
       # Should also do the same for the biases!
@@ -245,23 +239,24 @@ def main(argv):
       qstds = []
       for i, layer in enumerate(neural_net.layers):
         q = layer.kernel_posterior
+        q_b = layer.bias_posterior
         names.append("Layer {}".format(i))
         qmeans.append(q.mean())
+        qmeans.append(q_b.mean())
         qstds.append(q.stddev())
+        qstds.append(q_b.stddev())
 
       # weights_distribution = tfd.MultivariateNormalDiag(loc=qmeans, scale=qstds)
 
-
-
     # Compute the -ELBO as the loss, averaged over the batch size.
-    neg_log_likelihood = -tf.reduce_mean(labels_distribution.log_prob(labels))
-    kl = sum(neural_net.losses) / mnist_data.train.num_examples
+    neg_log_likelihood = -tf.reduce_mean(tf.squared_difference(predictions, labels))
+    kl = sum(neural_net.losses) / train_range # mnist_data.train.num_examples
     elbo_loss = neg_log_likelihood + kl
 
     # Build metrics for evaluation. Predictions are formed from a single forward
     # pass of the probabilistic layers. They are cheap but noisy predictions.
     predictions = tf.argmax(logits, axis=1) # remove since 1 dimensional output
-    accuracy, accuracy_update_op = tf.metrics.accuracy( # chang to mean_squared_error
+    accuracy, accuracy_update_op = tf.metrics.mean_squared_error( # change to mean_squared_error
         labels=labels, predictions=predictions)
 
     with tf.name_scope("train"):
@@ -272,13 +267,17 @@ def main(argv):
       train_op = opt.minimize(elbo_loss)
       # start the session
       sess = tf.Session()
-      sess.run(tf.global_variables_initializer()) # not sure what this does?
-      sess.run(tf.local_variables_initializer()) # note sure what this does?
+      # initialize the variables
+      sess.run(tf.global_variables_initializer())
+      sess.run(tf.local_variables_initializer())
 
       # Run the training loop. ?? what is happening from here
       train_handle = sess.run(training_iterator.string_handle())
       heldout_handle = sess.run(heldout_iterator.string_handle())
 
+      i = tf.Variable(0, name="batch_i")
+      increment_i = tf.assign_add(i, 1)
+      
       # Run the epochs
       for epoch in range(FLAGS.max_epochs):
         _ = sess.run([train_op, accuracy_update_op],
@@ -292,7 +291,7 @@ def main(argv):
               epoch, loss_value, accuracy_value))
 
         # check if time to save vizualisations
-        if (epoch+1) % FLAGS.viz_epochs == 0:
+        '''if (epoch+1) % FLAGS.viz_epochs == 0:
           # Compute log prob of heldout set by averaging draws from the model:
           # p(heldout | train) = int_model p(heldout|model) p(model|train)
           #                   ~= 1/n * sum_{i=1}^n p(heldout | model_i)
@@ -321,7 +320,7 @@ def main(argv):
                                         FLAGS.model_dir,
                                         "epoch{:05d}_pred.png".format(epoch)),
                                     title="mean heldout logprob {:.2f}"
-                                    .format(heldout_lp))
+                                    .format(heldout_lp))'''
 
 if __name__ == "__main__":
   tf.app.run()
