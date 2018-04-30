ea814e3366323eff6bdecc0f2483844fd6d59bf0
csamuelsson <c.samuelsson94@gmail.com>
2018-04-30 02:02:45 +0200

    First working version

--- bayesianNN.py
+++ bayesianNN.py
@@ -45,7 +45,7 @@ IMAGE_SHAPE = [28, 28]
 
 # Tuning some of the hyperparameters manually:
 FLAGS = flags.FLAGS
-FLAGS.learning_rate = 0.5
+FLAGS.learning_rate = 0.01
 FLAGS.viz_epochs = 1000
 FLAGS.viz_enabled = False # set to false if we want to train the model faster
 FLAGS.max_epochs = 10000
@@ -122,21 +122,23 @@ train_percentage = 0.8
   print("saved {}".format(fname))'''
 
 
-def build_input_pipeline(drug_data_path, batch_size, heldout_size,
+def build_input_pipeline(drug_data_path, batch_size,
                           number_of_principal_components):
   """Build an Iterator switching between train and heldout data.
   Args:
     `drug_data`: string representing the path to the .npy dataset.
     `batch_size`: integer specifying the batch_size for the dataset.
-    `heldout_size`: ?
     `number_of_principal_components`: integer specifying how many principal components
     to reduce the dataset into.
   """
   # Build an iterator over training batches.
   # Load the training data into two NumPy arrays, for example using `np.load()`.
-  with np.load("/data/drug_data.npz") as data:
+  with np.load(drug_data_path) as data:
     features = data["features"]
     labels = data["labels"]
+
+    assert not np.any(np.isnan(features))
+    assert not np.any(np.isnan(labels))
 
     # PCA (sklearn) and normalising
 
@@ -146,7 +148,7 @@ def build_input_pipeline(drug_data_path, batch_size, heldout_size,
     training_features = features[:train_range]
     training_labels = labels[:train_range]
     validation_features = features[train_range:]
-    validation_labels = validation[train_range:]
+    validation_labels = labels[train_range:]
 
   # Create the tf.Dataset object
   training_dataset = tf.data.Dataset.from_tensor_slices((training_features, training_labels))
@@ -160,8 +162,8 @@ def build_input_pipeline(drug_data_path, batch_size, heldout_size,
   # i.e., return the entire heldout set as a constant.
   heldout_dataset = tf.data.Dataset.from_tensor_slices(
       (validation_features, validation_labels))
-  heldout_frozen = (heldout_dataset.take(heldout_size).
-                    repeat().batch(heldout_size))
+  heldout_frozen = (heldout_dataset.take(len(validation_features)).
+                    repeat().batch(len(validation_features)))
   heldout_iterator = heldout_frozen.make_one_shot_iterator()
 
   # Combine these into a feedable iterator that can switch between training
@@ -171,7 +173,7 @@ def build_input_pipeline(drug_data_path, batch_size, heldout_size,
       handle, training_batches.output_types, training_batches.output_shapes)
   features_final, labels_final = feedable_iterator.get_next()
 
-  return features_final, labels_final, handle, training_iterator, heldout_iterator
+  return features_final, labels_final, handle, training_iterator, heldout_iterator, train_range
 
 
 def main(argv):
@@ -194,14 +196,15 @@ def main(argv):
   # define the graph
   with tf.Graph().as_default():
     # what's happening here?
-    (images, labels, handle,
-     training_iterator, heldout_iterator) = build_input_pipeline(
-         mnist_data, FLAGS.batch_size, mnist_data.validation.num_examples)
+    (features, labels, handle,
+     training_iterator, heldout_iterator, train_range) = build_input_pipeline(
+         "drug_data.npz", FLAGS.batch_size,
+         FLAGS.num_principal_components)
 
     # Building the Bayesian Neural Network. 
     # We are here using the Gaussian Reparametrization Trick
     # to compute the stochastic gradients as described in the paper
-    with tf.name_scope("bayesian_neural_net", values=[images]): # values arg??
+    with tf.name_scope("bayesian_neural_net", values=[features]): # values arg??
       neural_net = tf.keras.Sequential()
       for units in FLAGS.layer_sizes:
         layer = tfp.layers.DenseReparameterization(
@@ -218,9 +221,9 @@ def main(argv):
             )
         neural_net.add(layer)
       neural_net.add(tfp.layers.DenseReparameterization(
-        units=1, # change to 1
+        units=1, # one dimensional output
         activation=None,
-        trainable=True,
+        trainable=True, # i.e subject to optimization
         kernel_prior_fn=default_multivariate_normal_fn, # NormalDiag with hyperopt sigma
         kernel_posterior_fn=tfp.layers.default_mean_field_normal_fn(), # softplus(sigma)
         kernel_posterior_tensor_fn=lambda x: x.sample(),
@@ -229,8 +232,9 @@ def main(argv):
         bias_posterior_tensor_fn=lambda x: x.sample()
         ))
       # logits = neural_net(images) # remove
-      predictions = neural_net(images)
+      predictions = neural_net(features)
       # labels_distribution = tfd.Categorical(logits=logits) # remove
+      labels_distribution = tfd.Normal(loc=predictions, scale=[1.])
 
       # Extract weight posterior statistics
       # Should also do the same for the biases!
@@ -249,13 +253,13 @@ def main(argv):
       # weights_distribution = tfd.MultivariateNormalDiag(loc=qmeans, scale=qstds)
 
     # Compute the -ELBO as the loss, averaged over the batch size.
-    neg_log_likelihood = -tf.reduce_mean(tf.squared_difference(predictions, labels))
-    kl = sum(neural_net.losses) / train_range # mnist_data.train.num_examples
-    elbo_loss = neg_log_likelihood + kl
+    neg_log_likelihood = -tf.reduce_mean(labels_distribution.log_prob(labels))
+    kl = sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)) / train_range # mnist_data.train.num_examples
+    elbo_loss = kl + neg_log_likelihood
 
     # Build metrics for evaluation. Predictions are formed from a single forward
     # pass of the probabilistic layers. They are cheap but noisy predictions.
-    predictions = tf.argmax(logits, axis=1) # remove since 1 dimensional output
+    # predictions = tf.argmax(logits, axis=1) # remove since 1 dimensional output
     accuracy, accuracy_update_op = tf.metrics.mean_squared_error( # change to mean_squared_error
         labels=labels, predictions=predictions)
 
@@ -283,7 +287,6 @@ def main(argv):
         _ = sess.run([train_op, accuracy_update_op],
                      feed_dict={handle: train_handle})
 
-        # printing to console every hundred iteration
         if epoch % 100 == 0:
           loss_value, accuracy_value = sess.run(
               [elbo_loss, accuracy], feed_dict={handle: train_handle})
@@ -301,7 +304,7 @@ def main(argv):
                               for _ in range(FLAGS.num_monte_carlo)])
           mean_probs = np.mean(probs, axis=0)
 
-          image_vals, label_vals = sess.run((images, labels),
+          image_vals, label_vals = sess.run((features, labels),
                                             feed_dict={handle: heldout_handle}) #?
           heldout_lp = np.mean(np.log(mean_probs[np.arange(mean_probs.shape[0]),
                                                  label_vals.flatten()])) #?
