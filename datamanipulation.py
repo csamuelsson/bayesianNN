201e1308d6f8485d291b7a6df708793af4494e66
csamuelsson <c.samuelsson94@gmail.com>
2018-05-05 21:49:39 +0200

    Updated script to remove fully correlated features

--- datamanipulation.py
+++ datamanipulation.py
@@ -11,22 +11,23 @@ x = np.array(data[data.columns[1:-1]], dtype='float32')
 # since x_6850 -> x_6908 has variance 0 they don't contribute to our mapping, therefore we remove them here
 x = x[0:len(x), 0:6849]
 
+print("Original data dimensions:", x.shape)
+
+# Remove varuables with 100% correlation with another one
+corr_matrix = np.corrcoef(np.transpose(x))
+
+var_to_remove = []
+print("Checking for full correlation...")
+for i in range(corr_matrix.shape[0]):
+    for j in range(corr_matrix.shape[1]):
+        if j <= i:
+            continue # since symmetry
+        elif abs(corr_matrix[i][j]) == 1 & j not in var_to_remove:
+            var_to_remove.append(i)
+
+# print(var_to_remove)
+x = np.delete(x, var_to_remove, axis=1) # removing variables that are fully correlated with another one
+print("New dimension:", x.shape)
+
+# save the data to local file to be loaded later for training
 np.savez('drug_data.npz', features=x, labels=y)
-with np.load('drug_data.npz') as data:
-    x_new = data['features']
-    y_new = data['labels']
-
-# Test for NaNs
-assert not np.any(np.isnan(x_new))
-assert not np.any(np.isnan(y_new))
-
-# Test of equalness in features
-for records in zip(x, x_new):
-    for elem in zip(records[0], records[1]):
-        assert elem[0] == elem[1], "Found two unmatching feature elements"
-
-# Test of equalness in labels
-for elem in zip(y, y_new):
-    assert elem[0] == elem[1], "Found two unmatching label elements"
-
-print("All tests passed!")
