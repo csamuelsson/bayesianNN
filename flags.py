0cb31ddae3641ad2614d7960f85e15525fbc4bda
csamuelsson <c.samuelsson94@gmail.com>
2018-05-05 21:57:11 +0200

    Removed flags that previously was hyperparams, now in separate json

--- flags.py
+++ flags.py
@@ -1,26 +1,6 @@
 from absl import flags
 import os
 
-flags.DEFINE_float("learning_rate",
-    default=0.1,
-    help="Initial learning rate.")
-
-flags.DEFINE_integer("max_epochs",
-    default=6000,
-    help="Number of training epochs to run.")
-
-flags.DEFINE_list("layer_sizes",
-    default=["128", "128", "128"],
-    help="Comma-separated list denoting hidden units per layer.")
-
-flags.DEFINE_string("activation",
-    default="relu",
-    help="Activation function for all hidden layers.")
-
-flags.DEFINE_integer("batch_size",
-    default=44,
-    help="Batch size. Must divide evenly into dataset sizes.")
-
 flags.DEFINE_string("data_dir",
     default=os.path.join(os.getenv("TEST_TMPDIR", "/tmp"),
         "bayesian_neural_network/data"),
@@ -32,7 +12,7 @@ flags.DEFINE_string("model_dir",
     help="Directory to put the model's fit.") # ?
 
 flags.DEFINE_integer("viz_epochs",
-    default=400,
+    default=2000,
     help="Frequency at which save visualizations.")
 
 flags.DEFINE_boolean("viz_enabled",
@@ -42,23 +22,3 @@ flags.DEFINE_boolean("viz_enabled",
 flags.DEFINE_integer("num_monte_carlo",
     default=100,
     help="Network draws to compute predictive probabilities.")
-
-flags.DEFINE_float("prior_std",
-    default=1.0,
-    help="Standard deviation for the prior distribution over the weights")
-
-flags.DEFINE_float("prior_pi",
-    default=0.5,
-    help="Location parameter for the two gaussians that represents the scale mixture prior")
-
-flags.DEFINE_float("prior_sigma_1",
-    default=0.75,
-    help="Standard deviation for the first gaussian in the scale mixture prior")
-
-flags.DEFINE_float("prior_sigma_2",
-    default=0.1,
-    help="Standard deviation for the second gaussian in the scale mixture prior")
-
-flags.DEFINE_integer("num_principal_components",
-    default=200,
-    help="Number of principal components to reduce the dataset towards.")
