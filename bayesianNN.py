083639853511593ae966a09f529de578320fe30b
csamuelsson <c.samuelsson94@gmail.com>
2018-05-07 14:19:10 +0200

    Removed redundant parts

--- bayesianNN.py
+++ bayesianNN.py
@@ -44,76 +44,6 @@ FLAGS.viz_enabled = False # set to false if we want to train the model faster
 
 train_percentage = 0.8
 
-def plot_weight_posteriors(names, qm_vals, qs_vals, fname):
-  """Save a PNG plot with histograms of weight means and stddevs.
-  Args:
-    names: A Python `iterable` of `str` variable names.
-    qm_vals: A Python `iterable`, the same length as `names`,
-      whose elements are Numpy `array`s, of any shape, containing
-      posterior means of weight varibles.
-    qs_vals: A Python `iterable`, the same length as `names`,
-      whose elements are Numpy `array`s, of any shape, containing
-      posterior standard deviations of weight varibles.
-    fname: Python `str` filename to save the plot to.
-  """
-  fig = figure.Figure(figsize=(6, 3))
-  canvas = backend_agg.FigureCanvasAgg(fig)
-
-  ax = fig.add_subplot(1, 2, 1)
-  for n, qm in zip(names, qm_vals):
-    sns.distplot(qm.flatten(), ax=ax, label=n)
-  ax.set_title("weight means")
-  ax.set_xlim([-1.5, 1.5])
-  ax.set_ylim([0, 4.])
-  ax.legend()
-
-  ax = fig.add_subplot(1, 2, 2)
-  for n, qs in zip(names, qs_vals):
-    sns.distplot(qs.flatten(), ax=ax)
-  ax.set_title("weight stddevs")
-  ax.set_xlim([0, 1.])
-  ax.set_ylim([0, 25.])
-
-  fig.tight_layout()
-  canvas.print_figure(fname, format="png")
-  print("saved {}".format(fname))
-
-def plot_heldout_prediction(input_vals, probs,
-                            fname, n=10, title=""):
-  """Save a PNG plot visualizing posterior uncertainty on heldout data.
-  Args:
-    input_vals: A `float`-like Numpy `array` of shape
-      `[num_heldout] + IMAGE_SHAPE`, containing heldout input images.
-    probs: A `float`-like Numpy array of shape `[num_monte_carlo,
-      num_heldout, num_classes]` containing Monte Carlo samples of
-      class probabilities for each heldout sample.
-    fname: Python `str` filename to save the plot to.
-    n: Python `int` number of datapoints to vizualize.
-    title: Python `str` title for the plot.
-  """
-  fig = figure.Figure(figsize=(9, 3*n))
-  canvas = backend_agg.FigureCanvasAgg(fig)
-  for i in range(n):
-    ax = fig.add_subplot(n, 3, 3*i + 1)
-    ax.imshow(input_vals[i, :].reshape(IMAGE_SHAPE), interpolation="None")
-
-    ax = fig.add_subplot(n, 3, 3*i + 2)
-    for prob_sample in probs:
-      sns.barplot(np.arange(10), prob_sample[i, :], alpha=0.1, ax=ax)
-      ax.set_ylim([0, 1])
-    ax.set_title("posterior samples")
-
-    ax = fig.add_subplot(n, 3, 3*i + 3)
-    sns.barplot(np.arange(10), np.mean(probs[:, i, :], axis=0), ax=ax)
-    ax.set_ylim([0, 1])
-    ax.set_title("predictive probs")
-  fig.suptitle(title)
-  fig.tight_layout()
-
-  canvas.print_figure(fname, format="png")
-  print("saved {}".format(fname))
-
-
 def build_input_pipeline(drug_data_path, batch_size,
                           number_of_principal_components):
   """Build an Iterator switching between train and heldout data.
@@ -210,7 +140,7 @@ def main(argv):
         neural_net.add(layer)
       neural_net.add(tfp.layers.DenseReparameterization(
         units=1, # one dimensional output
-        activation=None,
+        activation=None, # since regression (outcome not)
         trainable=True, # i.e subject to optimization
         kernel_prior_fn=default_multivariate_normal_fn, # NormalDiag with hyperopt sigma
         kernel_posterior_fn=tfp.layers.default_mean_field_normal_fn(), # softplus(sigma)
@@ -220,9 +150,10 @@ def main(argv):
         bias_posterior_tensor_fn=lambda x: x.sample()
         ))
       predictions = neural_net(features)
-      labels_distribution = tfd.Normal(loc=predictions, scale=[3.0]) # change hyperparam
+      labels_distribution = tfd.Normal(loc=predictions, scale=[5.0]) # change hyperparam
 
       # Extract weight posterior statistics
+      # Currently not used
       names = []
       qmeans = []
       qstds = []
@@ -234,22 +165,9 @@ def main(argv):
         qmeans.append(q_b.mean())
         qstds.append(q.stddev())
         qstds.append(q_b.stddev())
-
-      # weights_distribution = tfd.MultivariateNormalDiag(loc=qmeans, scale=qstds)
-
-    # Compute (2^(M-i)/(2^M-1) where M is the mini-batch size, i current mini-batch index
-    # i = tf.placeholder(tf.float32, name="i")
-    # i = tf.Variable(1.0, tf.float32, name="i")
-    # numerator = tf.pow(2.0, tf.subtract(11.0, i))
-    # denominator = tf.subtract(tf.pow(2.0, 11.0), 1)
-    # pi = tf.divide(numerator, denominator)
-
-    #mprint(training_iterator._get_next_call_count)
     
     # Compute the -ELBO as the loss, averaged over the batch size.
-    # neg_log_likelihood = -tf.reduce_mean(labels_distribution.log_prob(labels))
     neg_log_likelihood = tf.reduce_mean(tf.squared_difference(predictions, labels))
-    # neg_log_likelihood = -tf.reduce_mean(tf.squared_difference(predictions, labels))
     kl = sum(neural_net.losses) / train_range
     elbo_loss = kl + neg_log_likelihood
 
@@ -273,9 +191,6 @@ def main(argv):
       # Run the training loop
       train_handle = sess.run(training_iterator.string_handle())
       heldout_handle = sess.run(heldout_iterator.string_handle())
-
-      # i = tf.Variable(0, name="batch_i")
-      # increment_i = tf.assign_add(i, 1)
       
       # Run the epochs
       for epoch in range(hyperparams['optimizer_params']['epochs']):
@@ -290,38 +205,6 @@ def main(argv):
           )
           print("Epoch: {:>3d} Loss: [{:.3f}, {:.3f}] Accuracy: [{:.3f}, {:.3f}]".format(
               epoch, loss_value, loss_value_validation, accuracy_value, accuracy_value_validation))
-
-        # check if time to save vizualisations
-        '''if (epoch+1) % FLAGS.viz_epochs == 0 & False:
-          # Compute log prob of heldout set by averaging draws from the model:
-          # p(heldout | train) = int_model p(heldout|model) p(model|train)
-          #                   ~= 1/n * sum_{i=1}^n p(heldout | model_i)
-          # where model_i is a draw from the posterior p(model|train).
-          probs = np.asarray([sess.run((labels_distribution.probs),
-                                       feed_dict={handle: heldout_handle}) # will need to change to multivariate normal
-                              for _ in range(FLAGS.num_monte_carlo)])
-          mean_probs = np.mean(probs, axis=0)
-
-          image_vals, label_vals = sess.run((features, labels),
-                                            feed_dict={handle: heldout_handle}) #?
-          heldout_lp = np.mean(np.log(mean_probs[np.arange(mean_probs.shape[0]),
-                                                 label_vals.flatten()])) #?
-          print(" ... Held-out nats: {:.3f}".format(heldout_lp))
-
-          qm_vals, qs_vals = sess.run((qmeans, qstds)) # variational posterior parameters
-
-          if HAS_SEABORN & FLAGS.viz_enabled: 
-            plot_weight_posteriors(names, qm_vals, qs_vals,
-                                   fname=os.path.join(
-                                       FLAGS.model_dir,
-                                       "epoch{:05d}_weights.png".format(epoch)))
-
-            plot_heldout_prediction(image_vals, probs,
-                                    fname=os.path.join(
-                                        FLAGS.model_dir,
-                                        "epoch{:05d}_pred.png".format(epoch)),
-                                    title="mean heldout logprob {:.2f}"
-                                    .format(heldout_lp))'''
 
 if __name__ == "__main__":
   tf.app.run()
